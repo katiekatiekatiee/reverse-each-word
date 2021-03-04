3999cb351dfcff4969dc4984c947357788fb0309
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-04 17:07:26 -0500

    commit tree d6787b8425c43b216857ef3757ea8a8d20895b08

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+# def reverse_each_word(string)
+#     new_array = []
+#     words_reversed = string.split(" ")
+    
+#     words_reversed.each do |word|
+#         new_array << word.reverse
+#     end
+#     new_array.join(" ")
+# end
+
+def reverse_each_word(string)
+    words_reversed = string.split(" ")
+    
+    new_array = words_reversed.collect do |word| 
+        word.reverse
+    end
+    new_array.join(" ")
+end
+
+     
+
